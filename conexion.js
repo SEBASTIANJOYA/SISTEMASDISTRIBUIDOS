a2291516d84ede3b46edb1b8d1f8b3ff3a1516c2
Orlando <ortchvinc@gmail.com>
2020-08-03 19:15:52 -0500

    Creacion de modulos de node js para conexion a mysql
    Creacion de un javascript para hacer la conexion del login con la base de datos

---
+++ conexion.js
@@ -0,0 +1,17 @@
+
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+let jConexion = {
+
+       host:"localhost", 
+       user:"root", 
+       password:"lolap38lola", 
+       database:"sistemasdistribuidas"
+
+}
+
+
