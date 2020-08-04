e949faa0b139259487126cab71b9448898705d50
Orlando <ortchvinc@gmail.com>
2020-08-04 16:06:43 -0500

    modificacion del javascript de conexion Agregacion de mas modulos de nodejs

--- node.js
+++ node.js
@@ -1,29 +1,22 @@
-let jConexion = {
-
-       host:"localhost", 
-       user:"root", 
-       password:"lolap38lola", 
-       database:"basedistribuidos"
-
-}
-
-
-var con = mysql.createConnection(jConexion ); 
-
-
-con.connect(function(error){                                                                                                 
-            try{ 
-              
-               if(error){ 
-
-                   console.log("Error al establecer la conexión a la BD -- " + error); 
-
-               
-                   console.log("Conexión exitosa"); 
-
-               } 
-           }
-           catch(x){ 
-               console.log("Contacto.agregarUsuario.connect --Error-- " + x); 
-           } 
+const mysql      = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'tate19042001',
+  database : 'sistemasdistribuidos',
+  port:3306
 });
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Errror de conexion: ' + err);
+    return;
+  }
+
+ console.log('Conexion hecha id ' + connection.threadId);
+});
+
